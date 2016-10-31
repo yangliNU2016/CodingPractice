995665d7cf9d6b2a313415dd94f374767b76871d
yangliNU2016 <yangli2016@u.northwestern.edu>
2016-10-31 15:03:38 -0500

    Move zero solution version two
    Space Complexity O(1) Time Complexity O(n)

--- MoveZero.java
+++ MoveZero.java
@@ -8,16 +8,34 @@ Note:
 
 public class Solution {
 	public void moveZeros(int[] nums) {
+		/*
+		solution version one:
 		// Use an arraylist to store all non-zero elements
 		ArrayList<Integer> arrlst = new ArrayList<Integer>();
 		for (int i = 0; i < nums.length; i++) {
 			if (nums[i] != 0) arrlst.add(nums[i]);
 		} 
+		
 		// Pull all the elements out from the arraylist and rewrite the array with them followed by 0s
 		for (int j = 0; j < nums.length; j++) {
 			if (j < arrlst.size()) {
 				nums[j] = arrlst.get(j);
 			} else nums[j] = 0;
+		}
+		*/
+		
+		// Use an integer to keep track of the index of the element to swap with a non-zero element
+		int index = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != 0) {
+				nums[index] = nums[i];
+				index++;
+			}
+		}
+		
+		// Rewrite the remaining part with zeros
+		for (int j = index; j < nums.length; j++) {
+			nums[j] = 0;
 		}
 	}
 }
