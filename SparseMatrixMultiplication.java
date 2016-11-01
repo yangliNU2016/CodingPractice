56de36c1d11542d3735fb68fceb359d73785fdf0
yangliNU2016 <yangli2016@u.northwestern.edu>
2016-11-01 17:24:45 -0500

    Fixed couple bugs
    Including add a return statement for solution "multiply" method and add
    semicolon for a statement, etcs.

--- SparseMatrixMultiplication.java
+++ SparseMatrixMultiplication.java
@@ -20,16 +20,18 @@ AB = |-1 0 3 | x | 0 0 0 | = |-7 0 3 |
 
 public class Solution {
 	public int[][] multiply(int[][] A, int[][] B) {	
-		int[][] ret = new int[][]();
+	    if(A == null || B == null) return null;
+		int[][] ret = new int[A.length][B[0].length];
 		for (int i = 0; i < ret.length; i++) {
 			for (int j = 0; j < ret[i].length; j++) {
 				ret[i][j] = rowByCol(A, B, i, j);
 			}
 		}
+		return ret;
 	}
 	
 	public int rowByCol(int[][] A, int[][] B, int rowIndex, int colIndex) {
-		int ret = 0
+		int ret = 0;
 		for(int k = 0; k < A[rowIndex].length; k++) {
 			ret += A[rowIndex][k] * B[k][colIndex]; 
 		}
