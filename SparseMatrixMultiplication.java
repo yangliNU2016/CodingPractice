c3e0c9090c340589b6eed3c59ec8b9d0386bd3d2
yangliNU2016 <yangli2016@u.northwestern.edu>
2016-11-01 17:20:43 -0500

    SparseMatrixMultiplication solution 1
    It is a very straightforward but not very efficient solution in terms of
    space and time complexity.
    Time complexity O(n^2)
    Space complexity O(n^2)
    I may work on reducing the number of calculations by detecting "0"
    calculations.

---
+++ SparseMatrixMultiplication.java
@@ -0,0 +1,38 @@
+/*
+Given two sparse matrices A and B, return the result of AB
+You may assume that A's column number is equal to B's row number
+Example:
+
+A = [
+[1, 0, 0],
+[-1, 0, 3]
+]
+
+B = [
+[7, 0, 0],
+[0, 0, 0],
+[0, 0, 1]
+]
+	 | 1 0 0 |   | 7 0 0 |   | 7 0 0 |
+AB = |-1 0 3 | x | 0 0 0 | = |-7 0 3 |
+				 | 0 0 1 |
+*/				 
+
+public class Solution {
+	public int[][] multiply(int[][] A, int[][] B) {	
+		int[][] ret = new int[][]();
+		for (int i = 0; i < ret.length; i++) {
+			for (int j = 0; j < ret[i].length; j++) {
+				ret[i][j] = rowByCol(A, B, i, j);
+			}
+		}
+	}
+	
+	public int rowByCol(int[][] A, int[][] B, int rowIndex, int colIndex) {
+		int ret = 0
+		for(int k = 0; k < A[rowIndex].length; k++) {
+			ret += A[rowIndex][k] * B[k][colIndex]; 
+		}
+		return ret;
+	}
+}
